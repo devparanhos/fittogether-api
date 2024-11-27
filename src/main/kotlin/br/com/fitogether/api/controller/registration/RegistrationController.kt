78f8aef5a63f66d92e1f361aeee136ddbcae7fdd
Lucas Santos de Souza <lecvsantos@gmail.com>
2024-11-27 00:41:42 -0300

    criando rota para upload de foto do usuário

--- src/main/kotlin/br/com/fitogether/api/controller/registration/RegistrationController.kt
+++ src/main/kotlin/br/com/fitogether/api/controller/registration/RegistrationController.kt
@@ -12,20 +12,24 @@ import br.com.fitogether.api.domain.dto.response.UserResponse
 import br.com.fitogether.api.domain.dto.response.screens.registration.exercise.GetRegistrationExerciseScreenResponse
 import br.com.fitogether.api.domain.dto.response.screens.registration.experience.GetRegistrationExperienceScreenResponse
 import br.com.fitogether.api.domain.dto.response.screens.registration.goal.GetRegistrationGoalsScreenResponse
+import br.com.fitogether.api.domain.service.aws.S3Service
 import br.com.fitogether.api.domain.service.screen.registration.ScreenRegistrationService
 import br.com.fitogether.api.domain.service.user.UserService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/registration")
 class RegistrationController(
     private val screenRegistrationService: ScreenRegistrationService,
-    private val userService: UserService
+    private val userService: UserService,
+    private val s3Service: S3Service
 ) : BaseController() {
 
     @GetMapping(value = ["screen/genders"])
@@ -152,6 +156,27 @@ class RegistrationController(
             userId = userId,
             useCase = {
                 userService.updateUserPreferences(userId = userId, preferences = preferencesRequest)
+            }
+        )
+    }
+
+    @PostMapping(value = ["{userId}/photo"])
+    fun uploadPhoto(
+        @PathVariable userId: Long,
+        @RequestParam("file") file: MultipartFile
+    ): ResponseEntity<String> {
+        if (file.isEmpty) {
+            return ResponseEntity.badRequest().body("A imagem não pode estar vazia")
+        }
+
+        if (!file.contentType?.startsWith("image/")!!) {
+            return ResponseEntity.badRequest().body("O arquivo enviado não é uma imagem")
+        }
+
+        return execute(
+            userId = userId,
+            useCase = {
+                s3Service.uploadFile(file, userId)
             }
         )
     }
---
+++ src/main/kotlin/br/com/fitogether/api/domain/service/aws/S3Service.kt
@@ -0,0 +1,76 @@
+package br.com.fitogether.api.domain.service.aws
+
+import br.com.fitogether.api.config.aws.AwsS3Properties
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.*
+
+@Service
+class S3Service(
+    private val s3Client: S3Client,
+    private val awsS3Properties: AwsS3Properties
+) {
+    fun uploadFile(file: MultipartFile, userId: Long): ResponseEntity<String> {
+
+        clearObjectsByFolder("users/$userId")
+
+        val fileName = "users/$userId/${System.currentTimeMillis()}-${file.originalFilename}"
+
+        try {
+            // monta o request para fazer o upload da foto
+            val request = PutObjectRequest.builder()
+                .bucket(awsS3Properties.bucketName)
+                .key(fileName)
+                .contentType(file.contentType)
+                .build()
+
+            // executa o upload e retorna a url da foto
+            s3Client.putObject(
+                request,
+                RequestBody.fromBytes(file.bytes)
+            )
+
+            val fileUrl = "https://${awsS3Properties.bucketName}.s3.${awsS3Properties.region}.amazonaws.com/$fileName"
+            return ResponseEntity.ok("Imagem enviada com sucesso: $fileUrl")
+        } catch (e: Exception) {
+            throw RuntimeException("Erro ao fazer upload para o S3: ${e.message}")
+        }
+    }
+
+    fun clearObjectsByFolder(folder: String) {
+        val bucketName = awsS3Properties.bucketName
+
+        try {
+            // lista os objetos dentro da pasta para apagar posteriormente
+            val listObjectsRequest = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(folder)
+                .build()
+
+            val listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest)
+
+            // Verifica se há objetos para excluir e percorre os objetos excluindo-os
+            if (listObjectsResponse.hasContents() && listObjectsResponse.contents().isNotEmpty()) {
+                val deleteObjectsRequest = DeleteObjectsRequest.builder()
+                    .bucket(bucketName)
+                    .delete(
+                        Delete.builder()
+                            .objects(
+                                listObjectsResponse.contents().map {
+                                    ObjectIdentifier.builder().key(it.key()).build()
+                                }
+                            )
+                            .build()
+                    )
+                    .build()
+
+                s3Client.deleteObjects(deleteObjectsRequest)
+            }
+        } catch (e: Exception) {
+            throw RuntimeException("Erro ao deletar objetos no prefixo $folder: ${e.message}", e)
+        }
+    }
+}
