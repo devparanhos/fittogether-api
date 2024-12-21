0162159e536dffe92258b8d109cd34e50e7dd4a5
Lucas Santos de Souza <lecvsantos@gmail.com>
2024-12-21 17:56:41 -0300

    refac

--- src/main/kotlin/br/com/fitogether/api/data/repository/exercise/ExercisePoolAnswerRepository.kt
+++ src/main/kotlin/br/com/fitogether/api/data/repository/exercise/ExercisePoolAnswerRepository.kt
@@ -2,7 +2,6 @@ package br.com.fitogether.api.data.repository.exercise
 
 import br.com.fitogether.api.data.entity.exercise.ExercisePoolAnswerEntity
 import br.com.fitogether.api.data.entity.exercise.ExercisePoolAnswerId
-import br.com.fitogether.api.data.entity.exercise.ExercisePoolEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ExercisePoolAnswerRepository : JpaRepository<ExercisePoolAnswerEntity, ExercisePoolAnswerId> {}
